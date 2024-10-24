ce5a8dc2c1746166cecd2ab11efe14427ae13c03
Varun Gopinath <varun.g.nath.official@gmail.com>
2024-10-24 10:03:43 -0400

    Home Page Modified

--- planeat-frontend/src/pages/HomePage.js
+++ planeat-frontend/src/pages/HomePage.js
@@ -11,8 +11,8 @@ function HomePage() {
           Explore new recipes, plan your meals, and get automated shopping lists tailored to your preferences.
         </p>
         <img 
-          src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Rustic%20Fresh%20Salad.jpeg?alt=media&token=bdccf90a-6f38-404c-a186-84cdf3413e85" 
-          alt="Rustic Fresh Salad" 
+          src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Asian-style%20Noodle%20Dish%20with%20Chicken%20and%20Vegetables.jpeg?alt=media&token=8dab3348-20c2-4842-a48a-f41fb13f5c91" 
+          alt="Asian Style Noodles" 
           className="img-fluid mb-4" 
           style={{ maxHeight: '400px', borderRadius: '10px' }} 
         />
@@ -25,8 +25,8 @@ function HomePage() {
           <div className="col-md-4">
             <div className="card mb-4">
               <img 
-                src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Rustic%20Fresh%20Salad.jpeg?alt=media&token=bdccf90a-6f38-404c-a186-84cdf3413e85" 
-                alt="Recipe 1" 
+                src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Refined%20Culinary%20Plate%20of%20Pasta.jpeg?alt=media&token=cb6c9ec7-677c-4464-b3ce-fff1f42fa84c" 
+                alt="Rustic Fresh Salad" 
                 className="card-img-top" 
               />
               <div className="card-body">
@@ -38,26 +38,26 @@ function HomePage() {
           <div className="col-md-4">
             <div className="card mb-4">
               <img 
-                src="<RECIPE_IMAGE_URL_FROM_FIREBASE_2>" 
-                alt="Recipe 2" 
+                src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Citrus%20Spectrum%20on%20Dark%20Wood.jpeg?alt=media&token=6480669a-5214-4b01-b138-298eb6d9d761" 
+                alt="Citrus Spectrum" 
                 className="card-img-top" 
               />
               <div className="card-body">
-                <h5 className="card-title">Recipe 2</h5>
-                <p className="card-text">A brief description of Recipe 2.</p>
+                <h5 className="card-title">Citrus Spectrum</h5>
+                <p className="card-text">A colorful spectrum of citrus fruits, perfect for fresh and zesty meals.</p>
               </div>
             </div>
           </div>
           <div className="col-md-4">
             <div className="card mb-4">
               <img 
-                src="<RECIPE_IMAGE_URL_FROM_FIREBASE_3>" 
-                alt="Recipe 3" 
+                src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Summer%20Refreshment%20Drinks.jpeg?alt=media&token=7e3672a7-9f9d-46db-93fc-f646ac128e3a" 
+                alt="Summer Refreshment Drinks" 
                 className="card-img-top" 
               />
               <div className="card-body">
-                <h5 className="card-title">Recipe 3</h5>
-                <p className="card-text">A brief description of Recipe 3.</p>
+                <h5 className="card-title">Summer Refreshment Drinks</h5>
+                <p className="card-text">Cool off with these refreshing summer drinks packed with flavor.</p>
               </div>
             </div>
           </div>
@@ -70,14 +70,32 @@ function HomePage() {
         <div className="row">
           <div className="col-md-4">
             <h5>Personalized Recipes</h5>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Mouthwatering%20Hamburger%20Close-Up.jpeg?alt=media&token=1f4decad-e279-4dbd-8ccb-fa323816823d"
+              alt="Mouthwatering Hamburger"
+              className="img-fluid mb-3"
+              style={{ maxHeight: '200px', borderRadius: '10px' }}
+            />
             <p>Get recipe recommendations based on your dietary preferences and nutritional goals.</p>
           </div>
           <div className="col-md-4">
             <h5>Meal Planner</h5>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Vibrant%20Fruit%20and%20Granola%20Bowl%20with%20Edible%20Flowers.jpeg?alt=media&token=a369fcfe-0c4d-43fb-9748-41fa6ba9eda9"
+              alt="A Vibrant Granola Bowl"
+              className="img-fluid mb-3"
+              style={{ maxHeight: '200px', borderRadius: '10px' }}
+            />
             <p>Easily plan your weekly meals and generate automatic shopping lists.</p>
           </div>
           <div className="col-md-4">
             <h5>Nutrition Tracking</h5>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/planeatscapstone.appspot.com/o/Serene%20Glass%20of%20Water.jpeg?alt=media&token=98fcdb47-5004-419b-92de-fe72652e465d"
+              alt="A Glass of Water"
+              className="img-fluid mb-3"
+              style={{ maxHeight: '200px', borderRadius: '10px' }}
+            />
             <p>Track your nutritional intake and set daily calorie and macronutrient goals.</p>
           </div>
         </div>
