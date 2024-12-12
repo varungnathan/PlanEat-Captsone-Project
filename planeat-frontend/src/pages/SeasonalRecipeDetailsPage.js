22b2dd6c60ed21ee7b636e31ec54b73b78248b4e
Varun Gopinath <varun.g.nath.official@gmail.com>
2024-12-12 16:22:10 -0500

    Css Created foe Seasonal

--- planeat-frontend/src/pages/SeasonalRecipeDetailsPage.js
+++ planeat-frontend/src/pages/SeasonalRecipeDetailsPage.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import '../pagestyles/SeasonalRecipeDetailsPage.css';
 
 function SeasonalRecipeDetailsPage() {
   const { id } = useParams();
--- planeat-frontend/src/pages/SeasonalRecipesPage.js
+++ planeat-frontend/src/pages/SeasonalRecipesPage.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-// import '../pagestyles/SeasonalRecipesPage.css';
+import '../pagestyles/SeasonalRecipesPage.css';
 
 function SeasonalRecipesPage() {
   const [recipes, setRecipes] = useState([]);
